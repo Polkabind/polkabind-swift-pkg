7d7f6a502c206a312bfa3453f1ef9e59e221bfdf
wassimans <wassim@wassimans.com>
2025-07-12 15:52:47 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+  name: "Polkabind",
+  platforms: [.iOS(.v13)],
+  products: [
+    .library(name: "Polkabind", targets: ["Polkabind"]),
+  ],
+  targets: [
+    .binaryTarget(name: "polkabindFFI", path: "polkabindFFI.xcframework"),
+    .target(name: "Polkabind", dependencies: ["polkabindFFI"]),
+  ]
+)
